e361fb772607a4d0547aaa656136019c81c852d6
kdi <kerwin.dahomey@gmail.com>
2022-12-04 13:28:37 -0600

    halfway done with inverse

---
+++ Binomial.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    let N:number = 1;
+
+    function expand(x: number, y: number, n: number): number {
+  let result = 0;
+
+  // Use the binomial theorem to compute the expansion
+  for (let i = 0; i <= n; i++) {
+    result += binomialCoefficient(n, i) * Math.pow(x, n - i) * Math.pow(y, i);
+  }
+
+  return result;
+}
+
+</script>
--- src/Matrices.svelte
+++ src/Matrices.svelte
@@ -7,16 +7,11 @@
     let N: number = 1;
     let operation: number;
     let check:boolean = false;
-    let C: Matrix;
     let A: Matrix = { rows: 0, cols: 0, data: [] };
     let B: Matrix = { rows: 0, cols: 0, data: [] };
 
-
-    function isButtonDisabled(field: number): boolean {
-        return !field;
-    }
     function handleChange(matrix: Matrix) {
-        return (event) => {
+        return (event:any) => {
             const { name, value } = event.target;
             const [row, col] = name.split('-').map(Number);
             matrix.data[row][col] = parseInt(value);
@@ -39,7 +34,6 @@
                 result.data[i][j] = a.data[i][j] + b.data[i][j];
             }
         }
-
         return result;
     }
 
@@ -59,25 +53,111 @@
                 result.data[i][j] = a.data[i][j] - b.data[i][j];
             }
         }
+        return result;
+    }
+
+    function matrixMultiplication(a: Matrix, b: Matrix): Matrix {
+        // Make sure the matrices are compatible for multiplication
+        if (a.cols !== b.rows) {
+            throw new Error('Cannot multiply matrices of incompatible sizes');
+        }
+
+        // Create a new matrix to store the result
+        const result: Matrix = { rows: a.rows, cols: b.cols, data: [] };
+
+        // Perform the multiplication
+        for (let i = 0; i < a.rows; i++) {
+            result.data[i] = [];
+            for (let j = 0; j < b.cols; j++) {
+                let sum = 0;
+                for (let k = 0; k < a.cols; k++) {
+                    sum += a.data[i][k] * b.data[k][j];
+                }
+                result.data[i][j] = sum;
+            }
+        }
+        return result;
+    }
+
+    function getSubMatrix(m: Matrix, row: number, col: number): Matrix {
+        // Create a new matrix to store the submatrix
+        const result: Matrix = { rows: m.rows - 1, cols: m.cols - 1, data: [] };
+
+        // Iterate over the elements of the original matrix, skipping the specified row and column
+        for (let i = 0; i < m.rows; i++) {
+            if (i === row) continue;
+            const newRow: number[] = [];
+            for (let j = 0; j < m.cols; j++) {
+                if (j === col) continue;
+                newRow.push(m.data[i][j]);
+            }
+            result.data.push(newRow);
+        }
 
         return result;
     }
 
-    function generateMatrices() {
-        check = true;
+    function det(m: Matrix): number {
+
+        if (m.rows === 1) return m.data[0][0];
+
+        // a*d - b*c
+        if (m.rows === 2) return m.data[0][0] * m.data[1][1] - m.data[0][1] * m.data[1][0];
+        
+        let result:number = 0;
+        for (let i = 0; i < m.cols; i++) 
+            result += Math.pow(-1, i) * m.data[0][i] * det(getSubMatrix(m, 0, i));
+        
+        return result;
     }
+
+    function transpose(m: Matrix): Matrix {
+        let result: Matrix = {rows: m.rows, cols: m.cols, data: []};
+        result.data = m.data.map(row => row.slice());
+        for ( let i = 0; i < result.rows; i++) {
+            for ( let j = 0; j < result.cols; j++) {
+                result.data[i][j] = m.data[j][i];
+            }
+        }
+        return result;
+    }
+    function inverse(m: Matrix): Matrix {
+        let result: Matrix = {rows: m.rows, cols: m.cols, data: []};
+        result.data = m.data.map(row => row.slice());
+        let determinant = det(m);
+        if ( determinant === 0 ) {
+            throw new Error("Not invertable");
+        }
+
+        // 1x1 case
+        if (result.cols === 1) {
+            result.data[0][0] = 1/(result.data[0][0]);
+            return result;
+        }
+        // 2x2
+        if (result.cols === 2) {
+            result.data[0][0] = m.data[1][1]/determinant;
+            result.data[0][1] = -m.data[0][1]/determinant;
+            result.data[1][0] = -m.data[1][0]/determinant;
+            result.data[1][1] = m.data[0][0]/determinant;
+        }
+        return result;
+    }
+
     $: A = { rows: N, cols: N, data: Array(N).fill(0).map(() => Array(N).fill(0)) };
     $: B = { rows: N, cols: N, data: Array(N).fill(0).map(() => Array(N).fill(0)) };
-    $: C = addition(A, B);
 </script>
 
 <div>
     <h2>Matrices</h2>
     <label for="intN">Enter size for matrices: </label>
-    <input type=number id="intN" size="2" bind:value={N} min=1 max=4>
+    <input disabled={check} type=number id="intN" size="2" bind:value={N} min=1 max=4>
     <h3> Matrix size: {N}x{N} </h3>
-    <button disabled={isButtonDisabled(N)} on:click={generateMatrices}>
+    <button disabled={check} on:click={() => {check = true;}}>
         Generate N x N matrices
+    </button>
+    <button disabled={!check} on:click={() => {check=false;}}>
+        Clear matrices
     </button>
 
     {#if check === true}
@@ -111,28 +191,101 @@
             det A and det B
         </button>
         <button on:click={()=> operation = 5}>
-            addition       Transpose of A and B
+            Transpose of A and B
         </button>
         <button on:click={()=> operation = 6}>
             Inverse of A and B
         </button>
         {#if operation === 1}
+            <h3> Result of A+B: </h3>
             {#each addition(A,B).data as row, i}
                 <div>
-                    {#each row as ij, j}
-                        {`${ij}  `}   
-                    {/each}
+                    <p> [
+                        {#each row as ij, j}
+                            {`${ij}  `}   
+                        {/each}
+                        ]</p> 
                 </div>
             {/each}
         {/if}
         {#if operation === 2}
+            <h3> Result of A-B: </h3>
             {#each subtraction(A,B).data as row, i}
                 <div>
-                    {#each row as ij, j}
-                        {`${ij}  `}   
-                    {/each}
+                    <p> [
+                        {#each row as ij, j}
+                            {`${ij}  `}   
+                        {/each}
+                        ]</p> 
                 </div>
             {/each}
+        {/if}
+        {#if operation === 3}
+            <h3> Product of A*B: </h3>
+            {#each matrixMultiplication(A,B).data as row, i}
+                <div>
+                    <p> [
+                        {#each row as ij, j}
+                            {`${ij}  `}   
+                        {/each}
+                        ]</p> 
+                </div>
+            {/each}
+        {/if}
+        {#if operation === 4 }
+            <div>
+                <h3>
+                    Determinant of A: {det(A)}
+                </h3>
+                <h3>
+                    Determinant of B: {det(B)}
+                </h3>
+            </div>
+        {/if}
+        {#if operation === 5}
+            <h3> Transpose of A: </h3>
+            {#each transpose(A).data as row, _}
+                <div>
+                    <p> [
+                        {#each row as ij, _}
+                            {`${ij}  `}   
+                        {/each}
+                        ]</p> 
+                </div>
+            {/each}
+                <h3> Transpose of B: </h3>
+            {#each transpose(B).data as row, _}
+                <div>
+                    <p> [
+                        {#each row as ij, _}
+                            {`${ij}  `}   
+                        {/each}
+                        ]</p> 
+                </div>
+            {/each}
+        {/if}
+        {#if operation === 6}
+            <h3> Inverse of Matrix A: </h3>
+            {#each inverse(A).data as row, _}
+                <div>
+                    <p> [
+                        {#each row as ij, _}
+                            {`${ij}  `}   
+                        {/each}
+                        ]</p> 
+                </div>
+            {/each}
+            <h3> Inverse of Matrix B: </h3>
+            {#each inverse(B).data as row, _}
+                <div>
+                    <p> [
+                        {#each row as ij, _}
+                            {`${ij}  `}   
+                        {/each}
+                        ]</p> 
+                </div>
+            {/each}
+    
         {/if}
     {/if}
 </div>
