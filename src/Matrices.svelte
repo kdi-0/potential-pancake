f31f2f6f3a53624fc82c1c1fa61afd469e79158e
kdi <kerwin.dahomey@gmail.com>
2022-12-03 22:13:05 -0600

    add and subq work

--- src/Matrices.svelte
+++ src/Matrices.svelte
@@ -7,6 +7,7 @@
     let N: number = 1;
     let operation: number;
     let check:boolean = false;
+    let C: Matrix;
     let A: Matrix = { rows: 0, cols: 0, data: [] };
     let B: Matrix = { rows: 0, cols: 0, data: [] };
 
@@ -18,12 +19,48 @@
         return (event) => {
             const { name, value } = event.target;
             const [row, col] = name.split('-').map(Number);
-            matrix.data[row][col] = value;
+            matrix.data[row][col] = parseInt(value);
         }
     }
 
-    function addition(a: Matrix, b: Matrix) {
+    function addition(a: Matrix, b: Matrix): Matrix{
+        // Make sure the matrices are the same size
+        if (a.rows !== b.rows || a.cols !== b.cols) {
+            throw new Error('Cannot add matrices of different sizes');
+        }
 
+        // Create a new matrix to store the result
+        const result: Matrix = { rows: a.rows, cols: a.cols, data: [] };
+
+        // Add the corresponding elements of the two matrices
+        for (let i = 0; i < a.rows; i++) {
+            result.data[i] = [];
+            for (let j = 0; j < a.cols; j++) {
+                result.data[i][j] = a.data[i][j] + b.data[i][j];
+            }
+        }
+
+        return result;
+    }
+
+    function subtraction(a: Matrix, b: Matrix): Matrix{
+        // Make sure the matrices are the same size
+        if (a.rows !== b.rows || a.cols !== b.cols) {
+            throw new Error('Cannot add matrices of different sizes');
+        }
+
+        // Create a new matrix to store the result
+        const result: Matrix = { rows: a.rows, cols: a.cols, data: [] };
+
+        // Add the corresponding elements of the two matrices
+        for (let i = 0; i < a.rows; i++) {
+            result.data[i] = [];
+            for (let j = 0; j < a.cols; j++) {
+                result.data[i][j] = a.data[i][j] - b.data[i][j];
+            }
+        }
+
+        return result;
     }
 
     function generateMatrices() {
@@ -31,6 +68,7 @@
     }
     $: A = { rows: N, cols: N, data: Array(N).fill(0).map(() => Array(N).fill(0)) };
     $: B = { rows: N, cols: N, data: Array(N).fill(0).map(() => Array(N).fill(0)) };
+    $: C = addition(A, B);
 </script>
 
 <div>
@@ -47,7 +85,7 @@
         {#each A.data as row, i}
             <div>
                 {#each row as _, j}
-                    <input type="number" name={`${i}-${j}`} bind:value={A.data[i][j]} on:input={handleChange(A)} style="max-width: 3em;"/>
+                    <input type=number name={`${i}-${j}`} bind:value={A.data[i][j]} on:input={handleChange(A)} style="max-width: 3em;"/>
                 {/each}
             </div>
         {/each}
@@ -56,7 +94,7 @@
         {#each B.data as row, i}
             <div>
                 {#each row as _, j}
-                    <input type="number" name={`${i}-${j}`} style="max-width: 3em;" bind:value={B.data[i][j]} on:input={handleChange(B)}/>
+                    <input type=number name={`${i}-${j}`} style="max-width: 3em;" bind:value={B.data[i][j]} on:input={handleChange(B)}/>
                 {/each}
             </div>
         {/each}
@@ -66,20 +104,35 @@
         <button on:click={()=> operation = 2}>
             A - B
         </button>
-        <button>
+        <button on:click={()=> operation = 3}>
             A * B
         </button>
-        <button>
+        <button on:click={()=> operation = 4}>
             det A and det B
         </button>
-        <button>
-            Transpose of A and B
+        <button on:click={()=> operation = 5}>
+            addition       Transpose of A and B
         </button>
-        <button>
+        <button on:click={()=> operation = 6}>
             Inverse of A and B
         </button>
         {#if operation === 1}
-            <p>test</p>
+            {#each addition(A,B).data as row, i}
+                <div>
+                    {#each row as ij, j}
+                        {`${ij}  `}   
+                    {/each}
+                </div>
+            {/each}
+        {/if}
+        {#if operation === 2}
+            {#each subtraction(A,B).data as row, i}
+                <div>
+                    {#each row as ij, j}
+                        {`${ij}  `}   
+                    {/each}
+                </div>
+            {/each}
         {/if}
     {/if}
 </div>
