0397ddf6c509629dc973a2667e2ac06c9c58294c
kdi <kerwin.dahomey@gmail.com>
2022-12-04 19:21:17 -0600

    clean up

--- .svelte-kit/generated/nodes/0.js
+++
@@ -1,3 +0,0 @@
-import * as shared from "../../../src/routes/+layout.ts";
-export { shared };
-export { default as component } from "../../../node_modules/@sveltejs/kit/src/runtime/components/layout.svelte";
--- .svelte-kit/output/client/_app/immutable/chunks/1-b11782c4.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-11a31a1a.js";export{t as component};
